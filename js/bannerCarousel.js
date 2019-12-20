088d41aaa50468e4545a390c869f9d2f8a92cb33
haofullstack <hao9x0159@gmail.com>
2019-12-20 13:49:58 +0700

    first

---
+++ js/bannerCarousel.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    nav: true,
+    autoplayHoverPause: true
+  });
+});
